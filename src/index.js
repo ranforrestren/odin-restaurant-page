5c0e74e8ac236c179c168c260112c020c5ba5b7e
ranforrestren <ranforrestren@gmail.com>
2022-11-25 17:27:26 -0500

    Added basic HTML layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-alert("Test!");
+import './style.css';
--- webpack.config.js
+++ webpack.config.js
@@ -8,14 +8,14 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.css%/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+     {
+       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+       type: 'asset/resource',
+     },
     ],
   },
 };
