5fe7f54e4749d666acb6b0635154b057d9f04c28
ranforrestren <ranforrestren@gmail.com>
2022-11-26 20:44:02 -0500

    Switch to using javascript to populate DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import './style.css';
+import pizza from './pizza.jpg';
+import chefIcon from './chefIcon.jpg';
+
+const domManipulator = (() => {
+    const _root = document.querySelector('#content');
+
+    const addElement = function(type, id, parent, content = '') {
+        const element = document.createElement(type);
+        element.setAttribute('id', id);
+        if (type == 'img') 
+        {
+            element.src = content;
+        }
+        else
+        {
+            element.textContent = content;
+        }
+        parent.appendChild(element);
+        return element;
+    }
+
+    const initialSetup = function() {
+        const _header = addElement('div', 'header', _root);
+        addElement('h1', 'mainTitle', _header, 'Taste of Italy');
+        const _mainContent = addElement('div', 'mainContent', _root);
+        const _contentBox = addElement('div', 'contentBox', _mainContent);
+        addElement('p', 'tagline', _contentBox, "Your new favorite Italian restaurant.");
+        addElement('img', 'chefIcon', _contentBox, chefIcon);
+        addElement('p', 'bio', _contentBox, "Making pizza since 1978.");
+        addElement('div', 'footer', _root, "Made by Ran Ren");
+        addElement('img', 'background', _root, pizza);
+    }
+    return { addElement, initialSetup };
+})();
+
+domManipulator.initialSetup();
